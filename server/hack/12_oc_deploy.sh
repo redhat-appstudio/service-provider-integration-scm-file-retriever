1901cdffc48a96698f3bcab4644a2990b4cf391a
Sergii Kabashniuk <skabashniuk@redhat.com>
2022-02-01 09:39:50 +0200

    Basic for OpenShift

---
+++ server/hack/12_oc_deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+echo 'Deploying scm server with spi operator and oauth service'
+
+
+kustomize build server/config/os | oc apply -f -
+
+oc rollout status deployment/spi-scm-file-retriever-server  -n spi-scm
+oc rollout status deployment/spi-controller-manager  -n spi-system
+oc rollout status deployment/spi-oauth-service  -n spi-system
